5ae033cbfbd9deebfb7e910f19e3f69ebb099441
Luis Ferrufino <a2020115239@estudiantes.upsa.edu.bo>
2025-03-28 01:12:21 -0400

    feat: add company model and migration

---
+++ backend/MorenoSeguros/MorenoSeguros.Core/CompanyAggregate/Company.cs
@@ -0,0 +1,11 @@
+﻿using MorenoSeguros.Core.SharedKernel;
+using MorenoSeguros.Core.SharedKernel.Interfaces;
+
+namespace MorenoSeguros.Core.CompanyAggregate
+{
+    public class Company : BaseEntity, IAggregateRoot
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ backend/MorenoSeguros/MorenoSeguros.Infrastructure/Data/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MorenoSeguros.Core.CompanyAggregate;
+
+namespace MorenoSeguros.Infrastructure.Data.Configuration
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable(nameof(Company));
+            builder.HasKey(c => c.Id);
+        }
+    }
+}
