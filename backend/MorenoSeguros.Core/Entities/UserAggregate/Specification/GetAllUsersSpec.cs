ffc8acfbba64cff62d9d8f7b289a5dd4197a746e
JesusFerru <a2020115239@estudiantes.upsa.edu.bo>
2025-04-01 00:26:12 -0400

    feat: get all users

---
+++ backend/MorenoSeguros.Core/Entities/UserAggregate/Specification/GetAllUsersSpec.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+
+namespace MorenoSeguros.Core.Entities.UserAggregate.Specification;
+
+public class GetAllUsersSpec : Specification<User>
+{
+    public GetAllUsersSpec()
+    {
+        Query.OrderBy(u => u.FirstName);
+    }
+}
