b212cc215b8667afd3a6b89e38394a5e6bf8d310
JesusFerru <a2020115239@estudiantes.upsa.edu.bo>
2025-04-01 01:59:45 -0400

    feat: update user

---
+++ backend/MorenoSeguros.Api/Endpoints/UsersEndpoints/UpdateUser.UpdateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace MorenoSeguros.Api.Endpoints.UsersEndpoints
+{
+    public class UpdateUserCommand
+    {
+        public string Dni { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+
+}
