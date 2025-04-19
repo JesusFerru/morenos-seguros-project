d237b5ba256140a7ea28d710c14c6cdc05ee074e
Franklin-Pc <FRANK-PC@FRANK-PC>
2025-04-19 19:13:18 -0400

    fix packages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,7 +7,7 @@ import { LuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { provideFuse } from '@fuse';
 import { provideTransloco, TranslocoService } from '@jsverse/transloco';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, of } from 'rxjs';
 import { TranslocoHttpLoader } from './shared/infrastructure/helpers/transloco/transloco.http-loader';
 import { provideIcons } from './shared/infrastructure/helpers/icons/icons.provider';
 import { provideAuth } from './shared/infrastructure/interceptors/auth.provider';
@@ -66,14 +66,17 @@ export const appConfig: ApplicationConfig = {
           // Preload the default language before the app starts to prevent empty/jumping content
           provide   : APP_INITIALIZER,
           // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-          useFactory: () =>
-          {
-              const translocoService = inject(TranslocoService);
-              const defaultLang = translocoService.getDefaultLang();
-              translocoService.setActiveLang(defaultLang);
+          useFactory: () => {
+            const translocoService = inject(TranslocoService);
+            const defaultLang = translocoService.getDefaultLang();
+            translocoService.setActiveLang(defaultLang);
 
-              return () => firstValueFrom(translocoService.load(defaultLang));
-          },
+            return () =>
+                firstValueFrom(translocoService.load(defaultLang), {
+                    defaultValue: null
+                });
+
+        },
           multi     : true,
       },
 
