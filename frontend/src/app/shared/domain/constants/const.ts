74b78063074c8e36455abc1b1d72931975a20bcc
Franklin-Pc <FRANK-PC@FRANK-PC>
2025-10-06 00:16:36 -0400

    remove trash

--- frontend/src/app/shared/domain/constants/const.ts
+++ frontend/src/app/shared/domain/constants/const.ts
@@ -5,9 +5,9 @@ export const AUTH_NAME = 'authName';
 
 export const CREATE_SUCCESS = 'Registro creado exitosamente.';
 export const CREATE_ERROR = 'Error al crear el registro.';
-export const UPDATE_SUCCESS = 'Actualizado creado exitosamente.';
+export const UPDATE_SUCCESS = 'Actualizado exitosamente.';
 export const UPDATE_ERROR = 'Error al actualizar el registro.';
 
-export const DELETE_SUCCESS = 'Eleminacion exitosamente';
-export const DELETE_ERROR = 'Error al Eleminacion el registro.';
+export const DELETE_SUCCESS = 'Eliminación exitosa';
+export const DELETE_ERROR = 'Error al eliminar el registro.';
 
